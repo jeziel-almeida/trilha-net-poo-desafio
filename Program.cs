90ab521e1211d04f115bbf946fcd6af9eef36786
Jeziel Almeida <jeziel.almeida16@gmail.com>
2022-10-03 23:58:40 -0300

    Projeto concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("12345", "Modelo 1", "12345", 4);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Telegram");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone("54321", "Modelo 2", "54321", 2);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
